1ffcf419f9557c2573feb3138ac03c16ee9f0046
Your Name <keazikun@bk.ru>
2023-09-13 20:35:27 +0300

    ножницы бумага каменть

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-from collections import Counter #подсчёт букв(обьектов) в строке
-a = Counter(input("Введи слово: "))
+a = str(input("Начало игры в камень,ножницы,бумага.Первый игрок выбирает: "))
+b = str(input("Начало игры в камень,ножницы,бумага.Второй игрок выбирает: "))
+stone = "камень"
+paper = "бумага"
+scissors = "ножницы"
 
-print("Букв:",a)
+first = "победил первый игрок"
+two = "победил второй игрок"
+draw = "ничья"
+
+if a == stone and b == paper:
+    print(first)
+elif a == stone and b == scissors:
+    print(two)
+
+elif a == paper and b == stone:
+    print(two)
+elif a == paper and b == scissors:
+    print(first)
+
+elif a == scissors and b == paper:
+    print(first)
+elif a == scissors and b == stone:
+    print(two)
+
+
+
+
+elif a == b:
+    print(draw)
+else:
+    print("Введите заново")
+
+
+
